4a88562b24e99ec49de92a6f385db233d52a3045
ocawthorne <ocawthorne@gmail.com>
2020-12-08 18:55:07 +0000

    validations-lab-081220

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+   validates :title, presence: true
+   validates :content, length: { minimum: 50 }
+   validates :summary, length: { maximum: 15 }
+   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+   validate :is_clickbait?
+
+   CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
+  
+    def is_clickbait?
+      if CLICKBAIT_PATTERNS.none? { |p| p.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+
 end
